fdba1dd30c96ca8787e9867ccda9e6b69c4f1ba1
Marc Catrisse <marc.catrisse@upc.edu>
2022-05-17 23:40:34 +0200

    test spark / works!

--- persist_formatted.py
+++ persist_formatted.py
@@ -1,22 +1,60 @@
+from cgitb import scanvars
+import glob
 import json
+import os
 import time
+
+from pyspark.sql import SparkSession
+
 from models.datasource import Datasource
 from models.format_log_entry import FormatLogEntry
 
 from process import Process
+
 
 class FormatLoadProcess(Process):
     _log_collection_name = 'formatLog'
 
     def __init__(self):
         super().__init__()
-
+    
     def run_process(self):
+        # Enable database
         res = self._database.find('datasources', {})
         for datasource_str in res:
             datasource = Datasource(datasource_str)
             print('processing -> ' + datasource.name)
-            self._batch_load_format_inner(datasource)
+            self._batch_load_format(datasource)
+            # TODO remove this (only run one time for testing)
+            break
+
+    def _batch_load_format(self, datasource):
+        # Get files to process
+        files_list = self._hdfs_client.list(datasource.dest_path_landing)
+        # TODO Process all this in pyspark 
+        files_processed = self._get_files_processed()
+        files_list = self._get_files_to_process(files_list, files_processed, datasource.dest_path_landing)
+        files_list = list(filter(lambda item: item not in list(files_processed), files_list))
+        # Build pySpark pipeline for processing
+        spark = SparkSession.builder.master("local[*]").appName("datasource - " + datasource.name).getOrCreate()
+        #rdd = spark.sparkContext.parallelize([1,2,3])
+        #print(rdd.count())
+        rdd = spark.read.parquet(files_list[0])
+        print(rdd.count())
+
+    def _get_files_to_process(self, files_list, files_processed, dest_path_landing):
+        res = []
+        for file_name in files_list:
+            if file_name not in list(files_processed):
+                res.append('hdfs://dodrio.fib.upc.es:27000' + os.path.normpath(os.path.join(dest_path_landing, file_name)))
+        return res
+
+    def _get_files_processed(self):
+        loaded_files = set()
+        for item in self._database.find(self._log_collection_name, {}):
+            loaded_files.add(item['file_name'])
+        return loaded_files
+
     
     def _batch_load_format_inner(self, datasource):
         print(datasource.to_json())
